953eddd1d2353c8537cdf58a85340e759a87e6c7
Binu K Idicula <binu.idicula@outlook.com>
2017-09-27 01:06:20 +0530

    adding install command

--- src/com/netreally/bots/quiz/Hello.java
+++ src/com/netreally/bots/quiz/Hello.java
@@ -1,4 +1,14 @@
 package com.netreally.bots.quiz;
 
-public class Hello {
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class Hello implements RequestHandler<Integer, String>{
+
+
+    @Override
+    public String handleRequest(Integer myCount, Context context) {
+        return String.valueOf(myCount);
+    }
+
 }
